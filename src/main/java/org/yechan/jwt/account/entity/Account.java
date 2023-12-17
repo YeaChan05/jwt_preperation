b06a7009f3db320b428d7c50446d3cd090ad9059
YeaChan05 <qkenrdl05@gmail.com>
2023-12-17 14:34:18 +0900

    [FIX] Account가 UserDetails를 직접 구현하지 않도록 Adapter패턴 적용

--- src/main/java/org/yechan/jwt/account/entity/Account.java
+++ src/main/java/org/yechan/jwt/account/entity/Account.java
@@ -3,12 +3,9 @@ package org.yechan.jwt.account.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.yechan.jwt.account.entity.Authority;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Set;
 
 @Entity
@@ -18,66 +15,31 @@ import java.util.Set;
 @Getter
 @Setter
 @AllArgsConstructor
-public class Account implements UserDetails {
+public class Account {
     @Id
     @Column(name = "account_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    
     @Column(unique = true)
     private String username;
-
+    
     private String password;
-
+    
     private String phone;
     
     private LocalDateTime createdDateTime;
     
     private LocalDateTime modifiedDateTime;
-
+    
     @JsonIgnore
     @Column(name = "activated")
     private boolean activated;
-
+    
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "account_authority",
             joinColumns = {@JoinColumn(name = "account_id", referencedColumnName = "account_id")},
             inverseJoinColumns = {@JoinColumn(name = "authority_id", referencedColumnName = "role_type")})
     private Set<Authority> authorities;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<>(authorities);
-    }
-
-    @Override
-    public String getPassword() {
-        return this.password;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
---
+++ src/main/java/org/yechan/jwt/account/entity/AccountDetails.java
@@ -0,0 +1,52 @@
+package org.yechan.jwt.account.entity;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.Set;
+
+@Getter
+public class AccountDetails implements UserDetails {
+    private final Account account;
+
+    public AccountDetails(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return account.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return account.isActivated();
+    }
+    
+}
