7492eb1e5200864615dcd9092fbc0fde501056c7
YeaChan05 <qkenrdl05@gmail.com>
2023-12-19 19:41:15 +0900

    [ADD] redis를 사용한 토큰 블랙리스트 전략 및 refresh를 통한 토큰 재발급 로직 구현

--- src/main/java/org/yechan/jwt/account/AuthService.java
+++ src/main/java/org/yechan/jwt/account/AuthService.java
@@ -1,26 +1,51 @@
 package org.yechan.jwt.account;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.yechan.jwt.account.entity.AccountDetails;
 import org.yechan.jwt.token.TokenProvider;
 
+import java.time.Duration;
 import java.util.Collection;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final TokenProvider tokenProvider;
+    private final StringRedisTemplate redisTemplate;
+    
     public TokenInfo login(LoginBody loginBody) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginBody.getUsername(), loginBody.getPassword());
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        AccountDetails principal = (AccountDetails)authentication.getPrincipal();
+        AccountDetails principal = (AccountDetails) authentication.getPrincipal();
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        return tokenProvider.createToken(principal.getUsername(), authorities);
+        return tokenProvider.createTokens(principal.getUsername(), authorities);
+    }
+    @Transactional
+    public void logout(String refreshToken) throws IllegalAccessException {
+        if (!tokenProvider.validateToken(refreshToken)) {//만료된 토큰인 경우
+            log.info("토큰이 부적절합니다!");
+            throw new IllegalAccessException("is invalid token");
+        }
+        
+        Long leftSeconds = tokenProvider.getExpiredLeftSeconds(refreshToken);
+        log.info("토큰 만료까지 남은 시간: {}", leftSeconds.toString());
+        redisTemplate.opsForValue().set(refreshToken, "banned_token", Duration.ofSeconds(leftSeconds));
+    }
+    
+    public String refresh(String refreshToken) throws IllegalAccessException {
+        if (!tokenProvider.validateToken(refreshToken)) //만료된 토큰인 경우
+            throw new IllegalAccessException("is invalid token");
+        if(redisTemplate.opsForValue().get(refreshToken)!=null)
+            throw new IllegalAccessError("is banned token");
+        return tokenProvider.createNewToken(refreshToken);
     }
 }
