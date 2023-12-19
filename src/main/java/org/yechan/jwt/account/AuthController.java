c1ec6c72628c245d0b14606d73cf4537d39bd99e
YeaChan05 <qkenrdl05@gmail.com>
2023-12-19 19:40:05 +0900

    [FIX] 로그인 및 로그아웃 http header&body 수정, refresh 추가

--- src/main/java/org/yechan/jwt/account/AuthController.java
+++ src/main/java/org/yechan/jwt/account/AuthController.java
@@ -3,10 +3,10 @@ package org.yechan.jwt.account;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.yechan.jwt.token.TokenBlacklistService;
 
 @Slf4j
 @RestController
@@ -14,24 +14,35 @@ import org.yechan.jwt.token.TokenBlacklistService;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-    private final TokenBlacklistService tokenBlacklistService;
     
     @PostMapping("/login")
-    public ResponseEntity<TokenInfo> login(@Valid @RequestBody LoginBody loginBody) {
-        TokenInfo tokenInfo =authService.login(loginBody);
-        String grantType = tokenInfo.getGrantType();
+    public ResponseEntity<?> login(@Valid @RequestBody LoginBody loginBody) {
+        TokenInfo tokenInfo = authService.login(loginBody);
         String accessToken = tokenInfo.getAccessToken();
         String refreshToken = tokenInfo.getRefreshToken();
         return ResponseEntity.status(HttpStatus.OK)
-                .header("grantType",grantType)
-                .header("accessToken",accessToken)
-                .header("refreshToken",refreshToken).build();
+                .header(HttpHeaders.SET_COOKIE, refreshToken)
+                .body(accessToken);
     }
     
     @GetMapping("/logout")
-    public ResponseEntity<?> logout(@RequestHeader("accessToken") String accessToken,
-                                    @RequestHeader("refreshToken") String refreshToken) {
-        tokenBlacklistService.blacklistToken(accessToken,refreshToken);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> logout(@RequestHeader(HttpHeaders.SET_COOKIE) String refreshToken) {
+        try {
+            authService.logout(refreshToken);
+            return ResponseEntity.ok().build();
+        } catch (IllegalAccessException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+    
+    @GetMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestHeader(HttpHeaders.SET_COOKIE) String refreshToken) {
+        try {
+            String newAccessToken = authService.refresh(refreshToken);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(newAccessToken);
+        } catch (IllegalAccessException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
