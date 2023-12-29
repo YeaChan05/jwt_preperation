b8a9ebca450a211dcceab0fc3d8008816cdf3c2a
YeaChan05 <qkenrdl05@gmail.com>
2023-12-29 21:10:50 +0900

    [FIX] 응답 구조 및 메서드 변경 - header 변수를 body로 전환 및 get을 post로 전환

--- src/main/java/org/yechan/jwt/account/api/AuthController.java
+++ src/main/java/org/yechan/jwt/account/api/AuthController.java
@@ -3,13 +3,12 @@ package org.yechan.jwt.account.api;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.yechan.jwt.account.service.AuthService;
-import org.yechan.jwt.account.dto.LoginFormRequest;
 import org.yechan.jwt.account.dto.AuthenticationResponse;
+import org.yechan.jwt.account.dto.LoginFormRequest;
+import org.yechan.jwt.account.service.AuthService;
 
 @Slf4j
 @RestController
@@ -21,15 +20,12 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginFormRequest loginFormRequest) {
         AuthenticationResponse authenticationResponse = authService.login(loginFormRequest);
-        String accessToken = authenticationResponse.getAccessToken();
-        String refreshToken = authenticationResponse.getRefreshToken();
         return ResponseEntity.status(HttpStatus.OK)
-                .header(HttpHeaders.SET_COOKIE, refreshToken)
-                .body(accessToken);
+                .body(authenticationResponse);
     }
     
     @GetMapping("/logout")
-    public ResponseEntity<?> logout(@RequestHeader(HttpHeaders.SET_COOKIE) String refreshToken) {
+    public ResponseEntity<?> logout(@RequestHeader("refreshToken") String refreshToken) {
         try {
             authService.logout(refreshToken);
             return ResponseEntity.ok().build();
@@ -38,8 +34,8 @@ public class AuthController {
         }
     }
     
-    @GetMapping("/refresh")
-    public ResponseEntity<?> refresh(@RequestHeader(HttpHeaders.SET_COOKIE) String refreshToken) {
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestBody String refreshToken) {
         try {
             String newAccessToken = authService.refresh(refreshToken);
             return ResponseEntity.status(HttpStatus.OK)
