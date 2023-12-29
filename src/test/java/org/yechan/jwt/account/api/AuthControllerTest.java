872fddd10131188a020ab8ac0987f21acb4221ae
YeaChan05 <qkenrdl05@gmail.com>
2023-12-29 21:11:22 +0900

    [ADD] API를 통한 로그인 통합 테스트

---
+++ src/test/java/org/yechan/jwt/account/api/AuthControllerTest.java
@@ -0,0 +1,106 @@
+package org.yechan.jwt.account.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.yechan.jwt.account.config.JwtFilter;
+import org.yechan.jwt.account.dto.AuthenticationResponse;
+import org.yechan.jwt.account.dto.LoginFormRequest;
+import org.yechan.jwt.account.entity.Account;
+import org.yechan.jwt.account.repository.AccountRepository;
+import org.yechan.jwt.account.service.AuthService;
+import org.yechan.jwt.account.service.TokenProvider;
+import org.yechan.jwt.global.config.SecurityConfig;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(SecurityConfig.class)
+class AuthControllerTest {
+    MockMvc mockMvc;
+    
+    ObjectMapper objectMapper = new ObjectMapper();
+    
+    @Autowired
+    AccountRepository accountRepository;
+    
+    @MockBean
+    AuthService authService;
+    
+    @MockBean
+    TokenProvider tokenProvider;
+    @Autowired
+    JwtFilter jwtFilter;
+    
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
+    Account expectedAccount;
+    String password="qwe123";
+    
+    @Autowired
+    WebApplicationContext context;
+    
+    @BeforeEach
+    void setUp() {
+        
+        mockMvc= MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilters(jwtFilter)
+                .build();
+        
+        
+        LocalDateTime now = LocalDateTime.now();
+        expectedAccount = Account.builder()
+                .username("test")
+                .password(passwordEncoder.encode(password))
+                .phone("010-8744-5809")
+                .createdDateTime(now)
+                .modifiedDateTime(now)
+                .build();
+        accountRepository.save(expectedAccount);
+    }
+    
+    @Test
+    void login() throws Exception {
+        LoginFormRequest requestDto = LoginFormRequest.builder()
+                .username(expectedAccount.getUsername())
+                .password(password)
+                .build();
+        
+        given(authService.login(requestDto)).willReturn(new AuthenticationResponse("Bearer","access","refresh"));
+        
+        String reqBody = objectMapper.writeValueAsString(requestDto);
+        
+        
+        
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(reqBody))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(AuthenticationResponse.builder()
+                        .grantType("Bearer")
+                        .accessToken("access")
+                        .refreshToken("refresh")
+                        .build())))
+                .andDo(log());
+    }
+}
