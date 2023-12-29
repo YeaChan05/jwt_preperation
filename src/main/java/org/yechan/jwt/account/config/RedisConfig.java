1710da73c8b5488b0b8ff031b5a5620ba24d2586
YeaChan05 <qkenrdl05@gmail.com>
2023-12-29 15:15:41 +0900

    Revert "[FIX] redis 설정 수정"
    This reverts commit 3cbdc663b8de3b681950c6e9887d491dc66b6a39.

--- src/main/java/org/yechan/jwt/account/config/RedisConfig.java
+++ src/main/java/org/yechan/jwt/account/config/RedisConfig.java
@@ -1,34 +1,14 @@
 package org.yechan.jwt.account.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-@Profile({"deploy","test"})
+import org.springframework.data.redis.core.StringRedisTemplate;
+
 @Configuration
 public class RedisConfig {
-    
-    @Value("${spring.data.redis.host}")
-    private String host;
-    
-    @Value("${spring.data.redis.port}")
-    private int port;
-    
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-    
-    @Bean
-    public RedisTemplate<String, Integer> redisTemplate() {
-        RedisTemplate<String, Integer> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
     }
 }
