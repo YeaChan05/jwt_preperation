fbc81fd5c39cf3194f47b9740e21f489e321f8b1
YeaChan05 <qkenrdl05@gmail.com>
2024-01-01 19:02:01 +0900

    [FIX] type detect 가능하도록 getter 추가

--- src/main/java/org/yechan/jwt/account/dto/SignupResponse.java
+++ src/main/java/org/yechan/jwt/account/dto/SignupResponse.java
@@ -1,11 +1,13 @@
 package org.yechan.jwt.account.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
+@Getter
 public class SignupResponse {
     private String username;
     private Set<String> authorities;
