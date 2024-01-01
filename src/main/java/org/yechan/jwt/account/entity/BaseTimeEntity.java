14a12aa9782335fcb4bb1f68f6f97cedda976314
YeaChan05 <qkenrdl05@gmail.com>
2024-01-01 16:51:04 +0900

    [ADD] auditing을 위한 추상 엔티티 생성

---
+++ src/main/java/org/yechan/jwt/account/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package org.yechan.jwt.account.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity{
+    @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime createdDate;
+    
+    @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime modifiedDate;
+
+}
