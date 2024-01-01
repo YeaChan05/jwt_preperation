ff9b1676c50b69721dbcfc0cf60e1853af3d6658
YeaChan05 <qkenrdl05@gmail.com>
2024-01-01 18:53:46 +0900

    [FIX] Lazy initialize로 발생하는 문제 해결을 위해 fetch join 사용

--- src/main/java/org/yechan/jwt/account/repository/AccountRepository.java
+++ src/main/java/org/yechan/jwt/account/repository/AccountRepository.java
@@ -1,12 +1,17 @@
 package org.yechan.jwt.account.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.yechan.jwt.account.entity.Account;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
-    Optional<Account> findByUsername(String username);
-
-    Optional<Account> findOneWithAuthoritiesByUsername(String username);
+    
+    @Query("SELECT a FROM Account a " +
+            "LEFT JOIN FETCH a.accountAuthorities aa " +
+            "LEFT JOIN FETCH aa.authority " +
+            "WHERE a.username = :username")
+    Optional<Account> findOneWithAuthoritiesByUsername(@Param("username") String username);
 }
