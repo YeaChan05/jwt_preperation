24e187f77299e552b25059dd178d5b56f003ae8b
YeaChan05 <qkenrdl05@gmail.com>
2024-01-02 20:45:02 +0900

    [REMOVE] 불필요한 repository 삭제

--- src/main/java/org/yechan/jwt/account/repository/AccountAuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package org.yechan.jwt.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.yechan.jwt.account.entity.AccountAuthority;
-
-public interface AccountAuthorityRepository extends JpaRepository<AccountAuthority, Long> {
-}
