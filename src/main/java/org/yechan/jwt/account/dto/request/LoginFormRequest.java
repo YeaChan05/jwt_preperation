b9cb13c999968e723152325e9abd27a2bcd64aa8
YeaChan05 <qkenrdl05@gmail.com>
2024-01-02 20:48:55 +0900

    [FIX] DTO단에서 유효성 검증

--- src/main/java/org/yechan/jwt/account/dto/request/LoginFormRequest.java
+++ src/main/java/org/yechan/jwt/account/dto/request/LoginFormRequest.java
@@ -1,5 +1,6 @@
-package org.yechan.jwt.account.dto;
+package org.yechan.jwt.account.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
@@ -8,6 +9,8 @@ import lombok.extern.jackson.Jacksonized;
 @Builder
 @Jacksonized
 public class LoginFormRequest {
+    @NotBlank
     private final String username;
+    @NotBlank
     private final String password;
 }
--- src/main/java/org/yechan/jwt/account/dto/request/SignupRequest.java
+++ src/main/java/org/yechan/jwt/account/dto/request/SignupRequest.java
@@ -1,10 +1,12 @@
-package org.yechan.jwt.account.dto;
+package org.yechan.jwt.account.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.ToString;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
-import org.yechan.jwt.account.entity.Account;
+import org.yechan.jwt.account.domain.entity.Account;
 
 /**
  * DTO for {@link Account}
@@ -14,7 +16,10 @@ import org.yechan.jwt.account.entity.Account;
 @Builder
 @Jacksonized
 public class SignupRequest {
+    @NotBlank
     String username;
+    @NotBlank
     String password;
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
     String phone;
 }
